f620a42c7c43f37084100087bc8d9a81f78ef5e0
Santiago Eguidazu <eguidazu.san@gmail.com>
2023-04-27 13:27:36 -0300

    elimina la pagina home y renderiza directamente el juego

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Game from "./pages/Game";
 import "./styles/global.css";
 
-function App() {
-  return (
-    <Routes>
-      <Route index path="/memotest/" element={<Home />} />
-      <Route path="/memotest/game" element={<Game />} />
-    </Routes>
-  );
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Game />
   </React.StrictMode>
 );
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -14,18 +14,34 @@ function Game() {
 
   return (
     <Layout>
-      <h2 className={styles.title}>MEMOTEST PRINCESAS</h2>
+      {characters.length <= 0 && (
+        <>
+          <img
+            className={styles.logo}
+            src="disney-logo.png"
+            alt="Disney logo"
+          />
+
+          <h1 className={styles.title}>MEMOTEST</h1>
+          <h3 className={styles.text}>
+            Empecemos a descubrir algunas princesas de Disney!
+          </h3>
+        </>
+      )}
       {characters.length > 0 && (
-        <Board characters={characters} shuffleCharacters={shuffleCharacters} />
+        <>
+          <h2 className={styles.title}>MEMOTEST PRINCESAS</h2>
+          <Board
+            characters={characters}
+            shuffleCharacters={shuffleCharacters}
+          />
+        </>
       )}
       {characters.length <= 0 && (
         <button className={styles.start} onClick={() => shuffleCharacters()}>
           Comenzar juego
         </button>
       )}
-      <a className={styles.linkSticky} href="/memotest/">
-        volver al inicio
-      </a>
     </Layout>
   );
 }
